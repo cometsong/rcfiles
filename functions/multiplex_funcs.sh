4bfecfc9fce3755e7985509e6a5a08b960824908
cometsong <benjamin@cometsong.net>
2019-01-03 15:51:09 -0500

    func screen and tmux lists: refactor and simplify with no external function usage

--- functions/multiplex_funcs.sh
+++ functions/multiplex_funcs.sh
@@ -1,15 +1,16 @@
 # from screen-list.sh (more comments for readability in there)
 sclists() {
-    LL=$(screen -ls | \
-        grep "^[[:space:]][0-9]." | \
-        sed "s'\([0-9]*\)\.\([-_~.A-Za-z]*\).*\(..tached\).*'\2 \1 \3'" |\
-        sort 
-        )
+    LL=$(screen -ls \
+        | grep "^[[:space:]][0-9]." \
+        | sed "s'\([0-9]*\)\.\([-_~.A-Za-z]*\).*\(..tached\).*'\2 \1 \3'" \
+        | sort 
+        );
     if [[ "x$LL" != "x" ]]; then
         echo "Screen sessions"
-        listawkout "name pid status"
-        listawkout "------------ ------ ----------"
-        listawkout $LL
+        echo "name pid status" \
+             "------------ ------ ----------" \
+             "$LL" \
+            | xargs printf "%-12s %-6s %-10s\n";
     else 
         echo "  No screen sessions running."
     fi
@@ -17,14 +18,15 @@ sclists() {
 
 # from tmux-list.sh (more comments for readability in there)
 tmlists() {
-    CMD="tmux ls -F '#{session_name} #{session_windows} #{?session_attached,Attached,Detached}'"
-    LL=$(eval $CMD 2> /dev/null | sort)
+    local CMD="tmux ls -F '#{session_name} #{session_windows} #{?session_attached,Attached,Detached}'"
+    local LL=$(eval $CMD 2> /dev/null | sort)
 
     if [[  "x$LL" != "x" ]]; then
         echo "tmux sessions:"
-        listawkout "name wins status"
-        listawkout "------------ ------ ----------"
-        listawkout "$LL"
+        echo "name wins status" \
+             "------------ ------ ----------" \
+             "$LL" \
+            | xargs printf "%-12s %-6s %-10s\n";
     else 
         echo "  No tmux sessions running."
     fi
