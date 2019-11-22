171b8a21ac04a11b6a07510144ee6fc0b2b6e5cc
cometsong <benjamin@cometsong.net>
2019-11-22 11:57:48 -0500

    mv pip_upgrade func from aliases to functions/

--- functions/pip-show-requirements.sh
+++ functions/pip-show-requirements.sh
@@ -9,3 +9,16 @@ pip_show_reqs() {
 
 pip3_show_reqs() { pip_show_reqs 'pip3'; };
 #pip3_show_reqs
+
+pip_upgrade () {
+    pip=${1:-pip};
+    up_opts="--upgrade-strategy eager --progress-bar on";
+    ${pip} list -o &&      \
+    echo "Beginning upgrades of outdated packages..." && \
+    ${pip} list -o         \
+        | tail -n +3       \
+        | awk '{print $1}' \
+        | xargs -I{}       \
+            ${pip} install -U ${up_opts} {}
+}
+alias pip3_upgrade="pip_upgrade pip3"
