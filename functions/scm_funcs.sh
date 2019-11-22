81bf5e92e1ed370cc612a1d4a9126d83e6baeac2
cometsong <benjamin@cometsong.net>
2019-11-22 11:56:07 -0500

    add git func: gshow to show summaries of remotes, branches, status, commits, tags

--- functions/scm_funcs.sh
+++ functions/scm_funcs.sh
@@ -60,3 +60,24 @@ gpab() {
         git push --tags $R $current_branch
     done
 }
+
+gshow( ) {
+  # Summarize git details:
+  #   Remotes, Branches, Status, Logs (-$1), Tags
+  # unset/reset local core.pager (avoid 'less')
+  msgs="${1:-11}"
+  orig_pager="$(git config --local --get core.pager)" && \
+  git config core.pager '';
+  echo "${b_Rd} == Remotes == ${NORM}" \
+    && git remote -v;
+  echo "${b_Rd} == Branches == ${NORM}" \
+    && git branch -avv;
+  echo "${b_Rd} == Status == ${NORM}" \
+    && git status --short --branch;
+  echo "${b_Rd} == Log == ${NORM}" \
+    && git log --oneline --decorate --date=short -$msgs && \
+  echo "${b_Rd} == Tags == ${NORM}" \
+    && git describe --tags --long && \
+  git config --local core.pager "$orig_pager"
+}
+
